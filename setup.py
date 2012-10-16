dd14f611d9b9977db54f31b399cb6a190fcb598d
Collin Watson <collinwat@gmail.com>
2012-10-16 12:49:00 -0700

    Referencing pooldlib dev version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ required = [
     'PyYAML==3.10',
     'gunicorn==0.14.6',
     'yuicompressor==2.4.7',
-    'pooldlib==0.1.0-dev',
+    'pooldlib==0.1-dev.1350414213.f62c89c',
     'flask==0.9',
     'flask-gravatar==0.2.3',
     'flask-wtf==0.8',
