3fa69339ad0f21a7e6e0c0e23726159e747d7e9e
Brian Oldfield <brian@oldfield.io>
2012-12-14 14:29:34 -0800

    Updated pooldlib version.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ required = [
     'gunicorn==0.14.6',
     'yuicompressor==2.4.7',
     'itsdangerous==0.17',
-    'pooldlib==0.1-dev.1354219455.94bcc8d',
+    'pooldlib==0.1-dev.1355524002.1be5347',
     'flask==0.9',
     'flask-gravatar==0.2.3',
     'flask-login==0.1.3-json',
